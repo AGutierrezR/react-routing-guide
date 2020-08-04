3fffda4d1028fd95d630dbb222a09399bacc404f
Andres Gutierrez <agutierrez.codes@gmail.com>
2020-08-04 20:55:50 +0200

    Pasar Route Parametres
    Se puede pasar parametros de Route usando dos puntos (:) y el nombre del parametro
    
    Tomar en cuenta que si un parametro comparte similitud a otra ruta (como /new-post y /:id
    
    Es obligatorio colocar el Link con parametro despues

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,51 +2,53 @@ import React, { Component } from 'react'
 import Post from '../../../components/Post/Post'
 import './Post.css'
 import axios from '../../../axios'
+import { Link } from 'react-router-dom'
 
 class Posts extends Component {
   state = {
     posts: [],
   }
 
-  componentDidMount () {
-    axios.get('/posts')
-        .then(res => {
-            const posts = res.data.slice(0, 4)
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'Andres'
-                }
-            })
-            this.setState({
-                posts : updatedPosts
-            })
+  componentDidMount() {
+    axios
+      .get('/posts')
+      .then((res) => {
+        const posts = res.data.slice(0, 4)
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: 'Andres',
+          }
         })
-        .catch(err => {
-            console.log(err);
+        this.setState({
+          posts: updatedPosts,
         })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id})
-}
+    this.setState({ selectedPostId: id })
+  }
 
   render() {
-    let posts = <p style={{textAlign: 'center'}}>Something went wrong </p>
-    if(!this.state.error) {
-        posts = this.state.posts.map(post => {
-            return <Post
-                title={post.title} 
-                author={post.author} 
-                key={post.id}
-                clicked={() => this.postSelectedHandler(post.id)}/>
-        })
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong </p>
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
+        return (
+          <Link to={'/' + post.id} key={post.id}>
+            <Post
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)}
+            />
+          </Link>
+        )
+      })
     }
-    return (
-      <section className="Posts">
-        { posts }
-      </section>
-    )
+    return <section className="Posts">{posts}</section>
   }
 }
 
