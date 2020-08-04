96ae0bc2260fa8e89b34cdda3ff8b377a62ad683
Andres Gutierrez <agutierrez.codes@gmail.com>
2020-08-04 21:43:02 +0200

    Usar Switch para cargar una sola ruta

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import './Blog.css'
 import Posts from './Posts/Posts'
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
 import NewPost from './NewPost/NewPost'
 import FullPost from './FullPost/FullPost'
 
@@ -13,20 +13,32 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><NavLink to="/" exact>Home</NavLink></li>
-              <li><NavLink to={{
-                pathname: '/new-post',
-                hash: '#submit',
-                search: '?quit-submit=true'
-              }}>New Post</NavLink></li>
+              <li>
+                <NavLink to="/" exact>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quit-submit=true',
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
             </ul>
           </nav>
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     )
   }
